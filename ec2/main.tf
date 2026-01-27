adecd5e1fa20b1a5708424fd88d6111f9b88126d
revatighotkar <revatighotkar1999@gmail.com>
2026-01-27 14:55:37 +0530

    modified for values file

--- ec2/main.tf
+++ ec2/main.tf
@@ -1,6 +1,7 @@
-resource "aws_instance" "this" {
+resource "aws_instance" "MyEC2Instance" {
   ami           = var.ami_id
   instance_type = var.instance_type
+  key_name      = var.key_name
 
   tags = {
     Name = var.instance_name
--- ec2/variables.tf
+++ ec2/variables.tf
@@ -12,3 +12,10 @@ variable "instance_name" {
   description = "EC2 Name tag"
   type        = string
 }
+
+variable "key_name" {
+  description = "Key pair name for EC2 instance"
+  type        = string    
+  
+}
+

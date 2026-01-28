f4b35e40149d1d3e8574563be26b2cd02b613df6
revatighotkar <revatighotkar1999@gmail.com>
2026-01-28 14:30:33 +0530

    final

--- ec2/main.tf
+++ ec2/main.tf
@@ -2,6 +2,9 @@ resource "aws_instance" "MyEC2Instance" {
   ami           = var.ami_id
   instance_type = var.instance_type
   key_name      = var.key_name
+  vpc_security_group_ids      = var.security_group_ids
+  associate_public_ip_address = true
+  subnet_id                   = var.subnet_id
 
   tags = {
     Name = var.instance_name
--- ec2/variables.tf
+++ ec2/variables.tf
@@ -17,5 +17,15 @@ variable "key_name" {
   description = "Key pair name for EC2 instance"
   type        = string    
   
+
+
+}
+variable "security_group_ids" {
+  description = "List of security group IDs"
+  type        = list(string)
+}
+variable "subnet_id" {
+  description = "Subnet ID for the EC2 instance"
+  type        = string
 }
 
